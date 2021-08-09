942cdbc7da6ea0599de5a78a9410e451d74407d6
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-09 13:19:44 -0700

    add functions of different conversions Co-authored-by: Benjamin Wilson <benjaminw1030@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function celsiusToFahrenheit(Celsius) {
+	return ((Celsius * 1.8) + 32);
+}
+
+function fahrenheitToCelsius(Fahrenheit) {
+	return ((Fahrenheit-32) * (5/9));
+}
+
+function gallonsToLiters(gallons) {
+  return (gallons * 3.78541);
+}
+
+function litersToGallons(liters) {
+  return (liters * 0.264172);
+}
+
+function gramsToOunces(grams) {
+  return (grams * 0.035274);
+}
+
+function ouncesToGrams(ounces) {
+  return (ounces * 28.3495);
+}

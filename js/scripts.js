0529e0ea30c3398d946943682b8a2e7a2cea827c
Ben Wilson <benjaminw1030@gmail.com>
2021-08-11 14:36:31 -0700

    add user interface
    Co-authored-by: Anna Clarke <annac.klingberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,57 @@ function gramsToOunces(grams) {
 function ouncesToGrams(ounces) {
   return (ounces * 28.3495);
 }
+
+$(document).ready(function() {
+  $("form#CtoF").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#C").val());
+    const result = celsiusToFahrenheit(convert);
+    $(".CtoF-output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#FtoC").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#F").val());
+    const result = fahrenheitToCelsius(convert);
+    $(".FtoC-output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#GtoL").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#G").val());
+    const result = gallonsToLiters(convert);
+    $(".GtoL-output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#LtoG").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#L").val());
+    const result = litersToGallons(convert);
+    $(".LtoG-output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#GrtoO").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#Gr").val());
+    const result = gramsToOunces(convert);
+    $(".GrtoO-output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#OtoGr").submit(function(event) {
+    event.preventDefault();
+    const convert = parseInt($("#O").val());
+    const result = ouncesToGrams(convert);
+    $(".OtoGr-output").text(result);
+  });
+});
